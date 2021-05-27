3e7fe0f6a863bde26a4b305da41068d623ee6b77
Keiichi Hirobe <chalenge.akane@gmail.com>
2021-05-27 23:17:05 +0900

    Add 3.5

---
+++ cpp/3/3.5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    int cnt = 0;
+    bool ok = true;
+    cin >> N;
+    vector<int> a(N);
+
+    for (int i = 0; i < N; ++i)
+        cin >> a[i];
+
+    for (; ok; ++cnt)
+
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            if (a[i] % 2 == 0)
+            {
+                a[i] = a[i] / 2;
+            }
+            else
+            {
+                ok = false;
+
+                break;
+            }
+        }
+    }
+
+    cout << cnt << endl;
+}
