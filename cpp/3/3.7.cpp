871ee044e76269819f0fe68f1da1838b687bfca5
Keiichi Hirobe <chalenge.akane@gmail.com>
2021-05-29 09:53:43 +0900

    Add 3.7

---
+++ cpp/3/3.7.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long N;
+    cin >> N;
+    vector<int> a;
+    int len = 0;
+    long rest = N;
+    for (;;)
+    {
+        long n = rest % 10;
+        rest = rest / 10;
+        ++len;
+        a.push_back(n);
+        if (rest == 0)
+        {
+            break;
+        }
+    }
+    reverse(a.begin(), a.end());
+
+    long sum = 0;
+    for (int bit = 0; bit < (1 << (len - 1)); ++bit)
+    {
+        long t = 0;
+        for (int i = 0; i < len; ++i)
+        {
+            int shift = len - i - 1;
+            if ((bit & (1 << shift)) && i != 0)
+            {
+                sum += t;
+                t = a[i];
+            }
+            else
+            {
+                t = t * 10 + a[i];
+            }
+        }
+        sum += t;
+    }
+    cout << sum << endl;
+}
