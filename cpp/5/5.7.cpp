44a719b5cf3948a8397c227863d390ba15ded26c
Keiichi Hirobe <chalenge.akane@gmail.com>
2021-06-11 01:58:11 +0900

    https://atcoder.jp/contests/dp/submissions/23333959

---
+++ cpp/5/5.7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+void chmax(T &a, T b)
+{
+    if (a.length() < b.length())
+    {
+        a = b;
+    }
+}
+
+int main()
+{
+    string S, T;
+    cin >> S >> T;
+    vector<vector<string> > dp(S.size() + 1, vector<string>(T.size() + 1, ""));
+    for (unsigned int i = 0; i < S.size(); ++i)
+    {
+        for (unsigned int j = 0; j < T.size(); ++j)
+        {
+            if (S[i] == T[j])
+            {
+                chmax(dp[i + 1][j + 1], dp[i][j] + S[i]);
+            }
+            else
+            {
+                chmax(dp[i + 1][j + 1], dp[i][j]);
+                chmax(dp[i + 1][j + 1], dp[i][j + 1]);
+                chmax(dp[i + 1][j + 1], dp[i + 1][j]);
+            }
+        }
+    }
+    cout << dp[S.size()][T.size()] << endl;
+}
