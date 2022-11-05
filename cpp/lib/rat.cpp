df331a27864ed134716938b9e93bd135b963de63
Keiichi Hirobe <chalenge.akane@gmail.com>
2022-11-05 15:52:06 +0900

    add rat.cpp

---
+++ cpp/lib/rat.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+// clang-format off
+#define rep(i, s ,n) for(int i=s, i##_len=(n); i<i##_len; ++i)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+using ll = long long;
+// 2^60
+#define INF (1LL << 60)
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+using namespace std;
+using namespace atcoder;
+using Graph = vector<vector<int>>;
+template <typename T> ostream &operator<<(ostream &s, vector<vector<vector<T>>> const &v) { for (int i = 0; i < int(v.size()); ++i) { s << "[" << i << "]" << endl; s << v[i];} return s;}
+template <typename T> ostream &operator<<(ostream &s, vector<vector<T>> const &v) { for (int i = 0; i < int(v.size()); ++i ){ s << v[i];} return s;}
+template <typename T> ostream &operator<<(ostream &s, vector<T> const &v) { for (int i = 0; i < int(v.size()); ++i) { s << v[i]; if (i != int(v.size()) - 1) { s << ",";}} s << endl; return s;}
+// clang-format on
+
+struct Rat {
+    ll a, b;
+    // a/b
+    Rat(ll _a = 0, ll _b = 1) : a(_a), b(_b) {
+        if (b == 0) {
+            a = 1;
+            b = 0;
+            return;
+        }
+        if (b < 0)
+            b = -b, a = -a;
+        ll g = gcd(abs(a), b);
+        a /= g;
+        b /= g;
+    }
+    bool operator<(const Rat& x) const {
+        return a * x.b < x.a * b;
+    }
+};
