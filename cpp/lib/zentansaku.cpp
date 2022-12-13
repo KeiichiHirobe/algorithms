ab228a8930949d964ee5118a04288c2250862cf5
Keiichi Hirobe <chalenge.akane@gmail.com>
2022-12-13 10:50:03 +0900

    add lib

--- cpp/lib/zentansaku.cpp
+++ cpp/lib/zentansaku.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 #include <atcoder/all>
 // clang-format off
 #define rep(i, s ,n) for(int i=s, i##_len=(n); i<i##_len; ++i)
@@ -19,20 +20,16 @@ template <typename T> ostream &operator<<(ostream &s, vector<T> const &v) { for 
 
 // 参考問題: https://atcoder.jp/contests/abc268/submissions/34753666
 
-void solve()
-{
+void solve() {
     int n = 3;
     vector<int> A(n);
     // 合計で最大restを任意の要素に足せる時の配列の全探索
-    auto dfs = [&](auto &&f, vector<int> cur, int idx, int rest) -> void
-    {
-        if (idx == n)
-        {
+    auto dfs = [&](auto &&f, vector<int> cur, int idx, int rest) -> void {
+        if (idx == n) {
             cout << cur;
             return;
         }
-        if (rest)
-        {
+        if (rest) {
             ++A[idx];
             f(f, cur, idx, rest - 1);
             --A[idx];
@@ -68,29 +65,51 @@ void solve()
 
 // 同じ結果
 // こちらの方が再帰の種類が1種類なのでわかりやすいが汎用性は低いと思われる
-void solve2()
-{
+void solve2() {
     int n = 3;
     vector<int> A(n);
     // 合計で最大restを任意の要素に足せる時の配列の全探索
-    auto dfs = [&](auto &&f, vector<int> cur, int idx, int rest) -> void
-    {
-        if (idx == n)
-        {
+    auto dfs = [&](auto &&f, vector<int> &cur, int idx, int rest) -> void {
+        if (idx == n) {
             cout << cur;
             return;
         }
-        for (int i = rest; i >= 0; --i)
-        {
+        for (int i = rest; i >= 0; --i) {
             cur.push_back(A[idx] + i);
-            f(f, cur, idx+1, rest - i);
+            f(f, cur, idx + 1, rest - i);
             cur.pop_back();
         }
     };
-    dfs(dfs, {}, 0, 3);
+    vector<int> tmp;
+    dfs(dfs, tmp, 0, 3);
 }
 
-int main()
-{
+int main() {
     solve2();
+}
+
+// n^n の計算量がかかるが簡単にnext_permutationを実装できる
+// https://cpprefjp.github.io/reference/algorithm/next_permutation.html
+// 本家だと、n!*n
+void nextpermutation(int n) {
+    vector<int> cur;
+    vector<bool> used(n);
+
+    auto nextp = [&](auto &&f) -> void {
+        bool all = true;
+        rep(i, 0, n) {
+            if (not used[i]) {
+                all = false;
+                cur.push_back(i + 1);
+                used[i] = true;
+                f(f);
+                cur.pop_back();
+                used[i] = false;
+            }
+        }
+        if (all) {
+            cout << cur;
+        }
+    };
+    nextp(nextp);
 }
