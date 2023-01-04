984cb933ab5fe6a6ceb3c7b51b23da3486c82fa7
Keiichi Hirobe <chalenge.akane@gmail.com>
2023-01-05 01:20:31 +0900

    fix lib

--- cpp/lib/heiho_bunkatsu.cpp
+++ cpp/lib/heiho_bunkatsu.cpp
@@ -1,91 +1,130 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <map>
-#include <algorithm>
-#include <queue>
-#include <iomanip>
+#include <bits/stdc++.h>
+
 // clang-format off
-#define rep(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define rep(i, s ,n) for(int i=s, i##_len=(n); i<i##_len; ++i)
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-#define SZ(x) ((int)(x).size())
 using ll = long long;
 // 2^60
-const ll INF = 1LL << 60;
-// lower_bound(ALL(a), 4)
-#define ALL(a)  (a).begin(),(a).end()
-int gcd(int a,int b){return b?gcd(b,a%b):a;}
 int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
 using namespace std;
+using Graph = vector<vector<int>>;
+template <typename T> ostream &operator<<(ostream &s, vector<vector<vector<T>>> const &v) { for (int i = 0; i < int(v.size()); ++i) { s << "[" << i << "]" << endl; s << v[i];} return s;}
+template <typename T> ostream &operator<<(ostream &s, vector<vector<T>> const &v) { for (int i = 0; i < int(v.size()); ++i ){ s << v[i];} return s;}
+template <typename T> ostream &operator<<(ostream &s, vector<T> const &v) { for (int i = 0; i < int(v.size()); ++i) { s << v[i]; if (i != int(v.size()) - 1) { s << ",";}} s << endl; return s;}
 // clang-format on
 
+int INF = 2147483647;
+
 // https://kujira16.hateblo.jp/entry/2016/12/15/000000
-const int sqrtN = 512;
-struct SqrtDecomposition
-{
-    int N, K;
-    vector<int> data;
-    vector<int> bucketSum;
-    SqrtDecomposition(int n) : N(n)
-    {
-        K = (N + sqrtN - 1) / sqrtN;
-        data.assign(K * sqrtN, 0);
-        bucketSum.assign(K, 0);
+// 範囲に対する代入
+// 範囲に対するsum取得
+// に対応できる平方分割
+struct heiho {
+    int n;
+    // bucket count
+    int nb;
+    // 1 bucket size
+    int B;
+    vector<int> a;
+    vector<int> b;
+    // lazy update
+    vector<int> lb;
+
+    heiho(int n) : n(n) {
+        B = sqrt(n);
+        nb = (n + B - 1) / B;
+        a.assign(n, INF);
+        b.assign(nb, INF);
+        lb.assign(nb, -1);
     }
-    void add(int x, int y)
-    {
-        int k = x / sqrtN;
-        data[x] += y;
-        int sum = 0;
-        for (int i = k * sqrtN; i < (k + 1) * sqrtN; ++i)
-        {
-            sum += data[i];
+
+    // lazyをeval
+    void eval(int bi) {
+        if (lb[bi] == -1)
+            return;
+        for (int i = bi * B; i < (bi + 1) * B; ++i) {
+            a[i] = lb[bi];
         }
-        bucketSum[k] = sum;
+        lb[bi] = -1;
     }
-    // [x, y)
-    ll getSum(int x, int y)
-    {
-        ll sum = 0;
-        for (int k = 0; k < K; ++k)
-        {
-            int l = k * sqrtN, r = (k + 1) * sqrtN;
-            if (r <= x || y <= l)
-                continue;
-            if (x <= l && r <= y)
-            {
-                sum += bucketSum[k];
-            }
-            else
-            {
-                for (int i = max(x, l); i < min(y, r); ++i)
-                {
-                    sum += data[i];
-                }
-            }
+
+    // bucketをeval
+    void evalBucket(int bi) {
+        if (lb[bi] != -1)
+            return;
+        int ans = INF;
+        for (int i = bi * B; i < (bi + 1) * B; ++i) {
+            chmin(ans, a[i]);
         }
-        return sum;
+        b[bi] = ans;
+    }
+
+    // [l,r)
+    void update(int l, int r, int x) {
+        eval(l / B);
+        eval((r - 1) / B);
+        int tl = l;
+        int tr = r;
+        while (tl < tr and tl % B != 0) {
+            a[tl] = x;
+            ++tl;
+        }
+        while (tl < tr and tr % B != 0) {
+            --tr;
+            a[tr] = x;
+        }
+        while (tl < tr) {
+            b[tl / B] = x;
+            lb[tl / B] = x;
+            tl += B;
+        }
+        evalBucket(l / B);
+        evalBucket((r - 1) / B);
+    }
+
+    int query(int l, int r) {
+        eval(l / B);
+        eval((r - 1) / B);
+        int tl = l;
+        int tr = r;
+        int ans = INF;
+        while (tl < tr and tl % B != 0) {
+            chmin(ans, a[tl]);
+            ++tl;
+        }
+        while (tl < tr and tr % B != 0) {
+            --tr;
+            chmin(ans, a[tr]);
+        }
+        // tl,trはbucketの左端
+        while (tl < tr) {
+            chmin(ans, b[tl / B]);
+            tl += B;
+        }
+        return ans;
     }
 };
 
-signed main()
-{
-    int N, Q;
-    cin >> N >> Q;
-    SqrtDecomposition decomp(N);
-    while (Q--)
-    {
-        int c, x, y;
-        cin >> c >> x >> y;
-        if (c == 0)
-        {
-            decomp.add(x - 1, y);
-        }
-        else
-        {
-            cout << decomp.getSum(x - 1, y) << endl;
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    heiho hf(n);
+    rep(i, 0, q) {
+        int c;
+        cin >> c;
+        if (c == 0) {
+            int l, r, x;
+            cin >> l >> r >> x;
+            ++r;
+            hf.update(l, r, x);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            ++r;
+            cout << hf.query(l, r) << endl;
         }
     }
 }
