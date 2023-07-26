9505feaed74a71192ee4017e939f7d7f7f596c31
Keiichi Hirobe <chalenge.akane@gmail.com>
2023-07-27 07:31:46 +0900

    update person.cpp

--- cpp/misc/person.cpp
+++ cpp/misc/person.cpp
@@ -5,15 +5,14 @@ struct person {
     person(int id = 0) : ptr(new int(id)) {
     }
     person(const person &p) : ptr(new int(*p.ptr)) {
-    if (ptr == nullptr) {
+        if (ptr == nullptr) {
             std::cout << "copy constructor for person (nullptr)" << std::endl;
         } else {
             std::cout << "copy constructor for person (" << *ptr << ")" << std::endl;
         }
-     
     }
     person(person &&p) : ptr(p.ptr) {
-        if (ptr == nullptr) {
+        if (p.ptr == nullptr) {
             std::cout << "get ownership for person (nullptr)" << std::endl;
         } else {
             std::cout << "get ownership for person (" << *ptr << ")" << std::endl;
@@ -29,12 +28,14 @@ struct person {
         delete ptr;
     }
     person &operator=(const person &p) {
+        std::cout << "copy assign" << std::endl;
         if (this != &p) {
             *ptr = *p.ptr;
         }
         return *this;
     }
     person &operator=(person &&p) {
+        std::cout << "move assign" << std::endl;
         delete ptr;
         ptr = p.ptr;
         p.ptr = nullptr;
@@ -61,3 +62,16 @@ struct person {
     }
 };
 
+void hoge(person p1, person p2, person &p3, person &&p4, person &&p5, person p6) {
+    std::cout << "hoge called" << std::endl;
+    std::cout << *p6.ptr << std::endl;
+}
+
+int main() {
+    person p1(1);
+    person p2(2);
+    person p3(3);
+    person p4(4);
+    hoge(p1, std::move(p2), p3, std::move(p4), person(5), person(6));
+    std::cout << "hoge called finished" << std::endl;
+}
